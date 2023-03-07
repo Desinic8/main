4183d74cee287844dc565f481b391babd6239123
Desinic8 <125421310+Desinic8@users.noreply.github.com>
2023-03-07 08:55:21 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  $dsn = "mysql:host={$_ENV["HOST"]};dbname={$_ENV["DATABASE"]}";
+  $options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
+
+  $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
+?>
